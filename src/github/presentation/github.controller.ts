5c7c0476e3905588800b68763c4632328770ac9a
Jhon Smitt <ext_jgomezc@falabella.cl>
2026-07-24 19:19:03 -0500

    fix(di): @Inject explicito en GithubController (esbuild sin decorator metadata)

--- src/github/presentation/github.controller.ts
+++ src/github/presentation/github.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { GetGithubUserUseCase } from '../application/use-cases/get-github-user.usecase';
 
 @Controller()
 export class GithubController {
-	constructor(private readonly getGithubUserUseCase: GetGithubUserUseCase) {}
+	constructor(
+		@Inject(GetGithubUserUseCase)
+		private readonly getGithubUserUseCase: GetGithubUserUseCase,
+	) {}
 
 	@Get('user/:username')
 	getUser(@Param('username') username: string) {
